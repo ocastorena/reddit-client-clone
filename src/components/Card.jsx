261ec514a0975bff730c891a3b732af8026ed376
ocastorena <omarcastorena11@gmail.com>
2025-01-26 13:35:30 -0700

    Move votes element in cards component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,9 +3,6 @@ import React from "react";
 const Card = ({ redditPost }) => {
   return (
     <article className="flex bg-dark p-4 rounded-lg shadow-md text-light ml-0">
-      <aside className="w-1/14 flex flex-col items-center">
-        <div>votes</div>
-      </aside>
       <div className="ml-5 mr-5 w-full">
         <header className="flex items-center space-x-4">
           <h4 className="font-bold">subreddit name</h4>
@@ -21,13 +18,13 @@ const Card = ({ redditPost }) => {
         </section>
         <div className="border-t border-gray-500 mt-5"></div>
         <footer className="flex items-center space-x-4 mt-5 text-gray-500">
+          <div>votes</div>
           <p>comments</p>
           <p>share</p>
           <p>save</p>
           <p>...</p>
         </footer>
       </div>
-      <aside className="w-1/14"></aside>
     </article>
   );
 };
